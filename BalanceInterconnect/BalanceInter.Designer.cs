40ca3c22ccfc2c0e07e0f164319643461179ac81
Alexey Pastukhov <sharpmsil@gmail.com>
2015-03-31 20:00:24 +0400

    MinimumButton = false.

--- BalanceInterconnect/BalanceInter.Designer.cs
+++ BalanceInterconnect/BalanceInter.Designer.cs
@@ -147,9 +147,10 @@
             this.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "FormMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Интерконнект";
+            this.Text = "Интерконнект баланс";
             this.contextMenuStrip.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
